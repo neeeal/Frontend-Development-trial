6d248fda261f6cfbed48923c90197fb34304add6
mikaeluhh <qmfepopes@tip.edu.ph>
2023-11-08 02:13:28 +0800

    scan page

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,68 +1,93 @@
-import React from 'react'
-import './Upload.css'
-import { useState } from 'react'
-import { MdCloudUpload, MdDelete} from 'react-icons/md'
-import { AiFillFileImage } from 'react-icons/ai'
+import React, { useState, useEffect } from 'react';
+import { MdCloudUpload } from 'react-icons/md';
+import { FaFire } from 'react-icons/fa';
+import './Upload.css';
+import BG from '../../assets/background.png';
 
 export default function Upload() {
+  const [image, setImage] = useState(null);
+  const [fileName, setFileName] = useState("No selected file");
+  const [classification, setClassification] = useState("Not yet classified");
+  const [recommendation, setRecommendation] = useState("No recommendation yet");
+  const [fireRating, setFireRating] = useState(0);
 
-  const [image, setImage] = useState(null)
-  const [fileName, setFileName] = useState("No selected file")  
+  const handleUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      setImage(URL.createObjectURL(file));
+    }
+  };
+
+  const handleRemove = () => {
+    setFileName("No selected file");
+    setImage(null);
+    setClassification("Not yet classified");
+    setRecommendation("No recommendation yet");
+    setFireRating(0);
+  };
+
+  const handleClassify = () => {
+    setClassification("Some classification result");
+    setRecommendation("Some recommendation result");
+    setFireRating(3);
+  };
+
+  useEffect(() => {
+    setClassification("Not yet classified");
+    setRecommendation("No recommendation yet");
+    setFireRating(0);
+  }, []);
+
   return (
-    <main>
-        <imageform 
-        onClick ={() => document.querySelector(".input-field").click()}
-        >
-            <input type="file" accept='image/*' className='input-field' hidden 
-            onChange={({ target: {files}}) => {
-                files[0] && setFileName(files[0].name)
-                if(files) {
-                    setImage(URL.createObjectURL(files[0]))
-                }
-            }}
-            
-            
-            
-            />
+    <section className='scan'>
+      <section>
+        <img src={BG} alt='BG' className='BG' />
+      </section>
+      <span className='scanTitle'>SCAN</span>
 
-            {image ?
-            <img src={image} style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "contain" }} alt={fileName} />
-            :
-            <>
-            <MdCloudUpload color='#1475cf' size={60}/>
-            <p>Upload an image</p>
-            </>
-            
-        
-        }
-        </imageform>
+      <main className="scanContainer">
+        <div className="uploadSection">
+          <div className="image-form" onClick={() => document.querySelector(".input-field").click()}>
+            <input type="file" accept="image/*" className="input-field" onChange={handleUpload} />
+            {image ? (
+              <img src={image} alt={fileName} />
+            ) : (
+              <div className="upload-placeholder">
+                <MdCloudUpload color="#1475cf" size={60} /><br />
+                <span className='formParagraph'>Upload an image</span>
+              </div>
+            )}
+          </div>
 
-        <section className='filename-upload'>
-            <AiFillFileImage color ='#1475cf' />
-            <span className='filecontent'>
-                {fileName} | 
-                <MdDelete 
-                onClick={() => {
-                    setFileName("No selected file")
-                    setImage(null)
-                }}
-                />
+          <div className="buttons">
+            <button onClick={handleClassify}>Classify</button>
+            <button onClick={handleRemove}>Remove</button>
+          </div>
+        </div>
+
+        <div className="resultsSection">
+          <span className='headerStyle'>Classification</span><br />
+          <div className="fire-icon-container">
+            <span className='paragraphStyle'>
+              {classification}
             </span>
-        </section>
-
-        <section className='results'>
-        <h4>Classification</h4>
-            <span className='resultscontent'>
-                    {fileName} 
-                </span>
-        </section>
-
-        <section className='results'>
-        <h4>Recommendation</h4>
-            <span className='resultscontent'>
-                    {fileName} 
-                </span>
-        </section>
-    </main>
-  )
+            <section className='fireCon'>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <FaFire
+                  key={index}
+                  color={index < fireRating ? 'red' : 'gray'}
+                  size={24}
+                  className="fire-icon"
+                />
+              ))}
+            </section>
+          </div>
+          <br /><br />
+          <span className='headerStyle'>Recommendation</span><br />
+          <span className='paragraphStyle'>{recommendation}</span><br />
+        </div>
+      </main>
+    </section>
+  );
 }
