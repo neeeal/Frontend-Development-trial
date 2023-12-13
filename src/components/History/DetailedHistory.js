823f71672aa7062947e892c5c86347b266ec9c6f
janeblancaocampo <qjbaocampo@tip.edu.ph>
2023-12-13 16:59:12 +0800

    added detailed history

---
+++ src/components/History/DetailedHistory.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './DetailedHistory.css';
+import placeholder_img from '../../../src/assets/sheat.png';
+
+function DetailedHistory() {
+  return (
+    <div>
+      <div className="header">
+        <h1>HISTORY</h1>
+      </div>
+
+      <div className="container">
+        <div className = "class">
+            <h2>Scan #1: </h2>
+            <p>Biotic Stress: Sheath Blight</p>
+        </div>
+        <div className="content">
+          <div className="image">
+            <img src={placeholder_img} alt="Placeholder" />
+          </div>
+          <div className="classification">
+            <h2>Classification</h2>
+            <p>Place your classification text here...</p>
+          </div>
+
+          <div className="recommendation">
+            <h2>Recommendation</h2>
+            <p>Place your recommendation text here...</p>
+          </div>
+
+          <div className="references">
+            <h2>References</h2>
+            <p>Place your references text here...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DetailedHistory;
